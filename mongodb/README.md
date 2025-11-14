0c51ff5402d01befcec1eacbe0a4900264ccd402
Vo Tran Phi <songokupkj@gmail.com>
2025-11-14 14:11:45 +0700

    refactor(v2): reset docker-compose.yml

--- mongodb/README.md
+++
@@ -1,9 +0,0 @@
-# How to run
-```bash
-cd mongodb
-
-openssl rand -base64 756 > mongo-keyfile
-chmod 400 mongo-keyfile
-
-docker compose up -d
-```
--- mongodb/init-mongo.js
+++
@@ -1,9 +0,0 @@
-db = db.getSiblingDB("admin");
-
-db.createUser({
-  user: process.env.MONGO_INITDB_ROOT_USERNAME,
-  pwd: process.env.MONGO_INITDB_ROOT_PASSWORD,
-  roles: [
-    { role: "root", db: "admin" }
-  ]
-});
