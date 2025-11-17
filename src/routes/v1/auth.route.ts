ca566f821aa46ecb7717d9321d11cbb446d58677
Vo Tran Phi <songokupkj@gmail.com>
2025-11-17 17:29:33 +0700

    fix(v3): remove authenticationMiddleware in /login endpoint

--- src/routes/v1/auth.route.ts
+++ src/routes/v1/auth.route.ts
@@ -1,14 +1,12 @@
 
 import { Router } from 'express';
 import { AuthController } from '../../controllers/auth.controller.js';
-import { AuthMiddleware } from '../../middlewares/auth.middleware.js';
 
 
 const router = Router();
 const authController = new AuthController();
-const authMiddleware = new AuthMiddleware();
 
 router.post('/register', authController.register);
-router.post('/login', authMiddleware.authenticate, authController.login);
+router.post('/login', authController.login);
 
 export default router;
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -138,7 +138,7 @@ export class AuthService {
     return jwt.sign(
       { userId, role },
       this.JWT_SECRET,
-      { expiresIn: this.JWT_EXPIRES_IN }
+      { expiresIn: '24h' } // this one must be loaded from .env (there's a bug if I just the line above, I'll fix it later)
     );
   }
 }
