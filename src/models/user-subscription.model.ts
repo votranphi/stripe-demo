ec27a33a58a35cffbaad15142f087a927bfe52b1
Vo Tran Phi <songokupkj@gmail.com>
2025-11-27 14:02:12 +0700

    feat(v4): add UserSubscription's status

--- src/models/user-subscription.model.ts
+++ src/models/user-subscription.model.ts
@@ -1,10 +1,15 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
+export enum UserSubscriptionStatus {
+  ACTIVE = 'ACTIVE',
+  INACTIVE = 'INACTIVE',
+}
+
 export interface UserSubscription {
   id: string;
   userId: string;
   stripeSubscriptionId: string;
-  status: string;
+  status: UserSubscriptionStatus;
   currentPeriodEnd: Date;
   createdAt?: Date;
 }
@@ -13,7 +18,7 @@ export interface UserSubscriptionDocument extends Document {
   id: string;
   userId: string;
   stripeSubscriptionId: string;
-  status: string;
+  status: UserSubscriptionStatus;
   currentPeriodEnd: Date;
   createdAt?: Date;
 }
@@ -22,7 +27,7 @@ const userSubscriptionSchema = new Schema<UserSubscriptionDocument>({
   id: { type: String, required: true, unique: true },
   userId: { type: String, required: true, ref: 'User' },
   stripeSubscriptionId: { type: String, required: true },
-  status: { type: String, required: true },
+  status: { type: String, enum: Object.values(UserSubscriptionStatus), required: true },
   currentPeriodEnd: { type: Date, required: true }
 }, {
   timestamps: true
